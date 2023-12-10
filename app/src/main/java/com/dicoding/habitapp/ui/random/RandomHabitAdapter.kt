f0f0406e0d2b6ef839d2733819af9c84ba58c9a6
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-11 04:25:00 +0700

    TODO 14
    //TODO 14 : Create view and bind data to item view

--- app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
@@ -3,6 +3,9 @@ package com.dicoding.habitapp.ui.random
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
@@ -40,7 +43,27 @@ class RandomHabitAdapter(
         RecyclerView.ViewHolder(itemView) {
         //TODO 14 : Create view and bind data to item view
 
+            private val  tvTitle = itemView.findViewById<TextView>(R.id.pager_tv_title)
+            private val  tvStartTime = itemView.findViewById<TextView>(R.id.pager_tv_start_time)
+            private val  ivPriority = itemView.findViewById<ImageView>(R.id.item_priority_level)
+            private val  tvMinutes = itemView.findViewById<TextView>(R.id.pager_tv_minutes)
+            private val  btnStartCountDown = itemView.findViewById<Button>(R.id.btn_open_count_down)
         fun bind(pageType: PageType, pageData: Habit) {
+
+            val color = when(pageType) {
+                PageType.HIGH -> R.drawable.ic_priority_high
+                PageType.MEDIUM -> R.drawable.ic_priority_medium
+                PageType.LOW -> R.drawable.ic_priority_low
+            }
+
+            tvTitle.text = pageData.title
+            tvMinutes.text = pageData.minutesFocus.toString()
+            tvStartTime.text = pageData.startTime
+            ivPriority.setImageResource(color)
+            btnStartCountDown.setOnClickListener{
+                onClick(pageData)
+            }
+
 
         }
     }
