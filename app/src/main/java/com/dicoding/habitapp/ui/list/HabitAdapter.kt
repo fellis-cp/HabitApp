2e063edabb8139b3d5d7208b85094c8f2358cfd1
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-11 02:46:03 +0700

    TODO 8 & 9
    //TODO 8 : Create and initialize ViewHolder
    
    //TODO 9 : Get data and bind them to ViewHolder

--- app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
@@ -1,5 +1,6 @@
 package com.dicoding.habitapp.ui.list
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
+import java.util.Locale
 
 class HabitAdapter(
     private val onClick: (Habit) -> Unit
@@ -16,11 +18,18 @@ class HabitAdapter(
 
     //TODO 8 : Create and initialize ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
-        throw NotImplementedError("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.habit_item,
+            parent ,
+            false
+        )
+        return HabitViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: HabitViewHolder, position: Int) {
         //TODO 9 : Get data and bind them to ViewHolder
+        val habit = getItem(position) as Habit
+        holder.bind(habit)
     }
 
     inner class HabitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -39,7 +48,19 @@ class HabitAdapter(
             itemView.setOnClickListener {
                 onClick(habit)
             }
+
+            val priority = when (habit.priorityLevel.lowercase(Locale.getDefault())){
+                "high"      -> R.drawable.ic_priority_high
+                "medium"    -> R.drawable.ic_priority_medium
+                "low"       -> R.drawable.ic_priority_low
+                else -> R.drawable.ic_priority_low
+            }
+
+            ivPriority.setImageResource(priority)
+
         }
+
+
 
     }
 
