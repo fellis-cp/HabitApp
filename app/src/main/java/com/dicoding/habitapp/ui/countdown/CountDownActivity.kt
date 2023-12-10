68233f5b5559c2de5503919fd63cd990a5169b74
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-11 02:48:34 +0700

    Todo 10
     //TODO 10 : Set initial time and observe current time. Update button state when countdown is finished

--- app/src/main/java/com/dicoding/habitapp/ui/countdown/CountDownActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/countdown/CountDownActivity.kt
@@ -25,6 +25,10 @@ class CountDownActivity : AppCompatActivity() {
             val viewModel = ViewModelProvider(this).get(CountDownViewModel::class.java)
 
             //TODO 10 : Set initial time and observe current time. Update button state when countdown is finished
+            viewModel.setInitialTime(habit.minutesFocus)
+            viewModel.currentTimeString.observe(this) {
+                findViewById<TextView>(R.id.tv_count_down).text = it
+            }
 
             //TODO 13 : Start and cancel One Time Request WorkManager to notify when time is up.
 
